a57da8ac280de60a491c025be21cfdc3005f6e8e
racheltsai2020 <66756662+racheltsai2020@users.noreply.github.com>
2021-11-17 21:38:32 -0800

    update with price

--- demo/src/main/java/demo/Order.java
+++ demo/src/main/java/demo/Order.java
@@ -1,12 +1,58 @@
 package demo;
 
 public class Order {
+	
+	
 	private String drinkName;
 	private String size;
 	private String temp;
 	private String milk;
 	private String topping;
+	private float price;
 	
+	public float calculatePrice() {
+		float totalPrice=0;
+		if(drinkName.equalsIgnoreCase("Black Coffee")) {
+			totalPrice+=4.25;
+		}
+		else if(drinkName.equalsIgnoreCase("Latte")) {
+			totalPrice+=4.50;
+		}
+		else if(drinkName.equalsIgnoreCase("Cold Brew")) {
+			totalPrice+=4.75;
+		}
+		else if(drinkName.equalsIgnoreCase("Hot Chocolate")) {
+			totalPrice+=4.25;
+		}
+		
+		if(size.equalsIgnoreCase("Medium")) {
+			totalPrice+=0.5;
+		}
+		else if(size.equalsIgnoreCase("Large")) {
+			totalPrice+=1;
+		}
+		
+		if(milk.equalsIgnoreCase("Soy Milk")) {
+			totalPrice+=0.2;
+		}
+		else if(milk.equalsIgnoreCase("Almond Milk")) {
+			totalPrice+=0.2;
+		}
+		price = totalPrice;
+		return price;
+	}
+	
+	
+	public float getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+
 	public String getTopping() {
 		return topping;
 	}
@@ -37,6 +83,8 @@ public class Order {
 	public void setSize(String size) {
 		this.size = size;
 	}
+	
+	
 	
 	@Override
 	public String toString() {
