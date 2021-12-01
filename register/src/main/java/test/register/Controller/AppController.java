e1df7c5b9678700caf477354a870df690f87fc8f
Walton Ma <walton.ma@SJSU.edu>
2021-11-30 20:33:59 -0800

    altered how obtaining user id/details

--- register/src/main/java/test/register/Controller/AppController.java
+++ register/src/main/java/test/register/Controller/AppController.java
@@ -14,82 +14,92 @@ import org.springframework.web.bind.annotation.PostMapping;
 import test.register.Model.Order;
 import test.register.Model.User;
 import test.register.Repository.UserRepository;
- 
-@Controller
-public class AppController {
- 
-    @Autowired
-    private UserRepository userRepo;
 
-    @Autowired
+@Controller
+public class AppController
+{
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
-     
+
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage()
+	{
 		return "index";
 	}
 
-    // @GetMapping("") 
+	// @GetMapping("")
 	// public String home() {
-	// 	return "homepage";
+	// return "homepage";
 	// }
 
-    @GetMapping("/menu")
-	public String menu() {
+	@GetMapping("/menu")
+	public String menu()
+	{
 		return "menu";
 	}
 
-    @GetMapping("/order")
-	public String showForm(Model model) {
+	@GetMapping("/order")
+	public String showForm(Model model)
+	{
 		Order ord = new Order();
 		model.addAttribute("order", ord);
 		return "order";
 	}
 
-    @GetMapping("/register")
-	public String showRegistrationForm(Model model) {
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model)
+	{
 		model.addAttribute("user", new User());
-		
+
 		return "signup_form";
 	}
-	
+
 	@PostMapping("/process_register")
-	public String processRegister(User user) {
+	public String processRegister(User user)
+	{
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String encodedPassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodedPassword);
-		
+
 		userRepo.save(user);
-		
+
 		return "register_success";
 	}
-	
+
 	@GetMapping("/users")
-	public String listUsers(Model model) {
+	public String listUsers(Model model)
+	{
 		List<User> listUsers = userRepo.findAll();
 		model.addAttribute("listUsers", listUsers);
-		
+
 		return "users";
 	}
 
 	@GetMapping("/history")
-	public String history() {
+	public String history()
+	{
 		return "order_history";
 	}
 
 	@PostMapping("/orderpage")
-	public String submitForm(@ModelAttribute("ord") Order ord) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		Long currentID = authentication.getID();
-		
+	public String submitForm(@ModelAttribute("ord") Order ord, @ModelAttribute("user") User user)
+	{
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		Long currentID = authentication.getID();
+
 		String orderSql = "INSERT INTO coffeeOrder " + "(drink_name, size, temperature, milk, topping, price, id) "
 				+ "VALUES(?,?,?,?,?,?,?)";
 		ord.calculatePrice();
-		int result = jdbcTemplate.update(sql, ord.getDrinkName(),ord.getSize(),ord.getTemp(),
-				ord.getMilk(), ord.getTopping(), ord.getPrice(), currentID);
+		int result = jdbcTemplate.update(orderSql, ord.getDrinkName(), ord.getSize(), ord.getTemp(), ord.getMilk(),
+				ord.getTopping(), ord.getPrice(), user.getId());
 		// user.getID() means put value of logged in user's ID
-		
-		if (result>0) {
+
+		if (result > 0)
+		{
 			System.out.println("new entry has been added");
 		}
 		return "order_success";
